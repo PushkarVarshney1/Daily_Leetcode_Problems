8940cf89a2ac710786d5f698a6a6e28bb72bfd59
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-08-21 12:09:22 +0530

    Time: 98 ms (70.72%) | Memory: 65.7 MB (20.45%) - LeetSync

--- 502-ipo/ipo.java
+++ 502-ipo/ipo.java
@@ -5,9 +5,13 @@ class Solution {
 			arr[i][0]=capital[i];
 			arr[i][1]=profits[i];
 		}
+        return IPO(arr,k,w);
+    }
+
+    public static int IPO(int [][]arr,int k , int w){
         Arrays.sort(arr,(a,b)->a[0]-b[0]);
 		PriorityQueue<int []> pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
-		for (int i = 0; i <= arr.length ; ) {
+		for (int i = 0; i < arr.length ; ) {
 			while(i<arr.length && w>= arr[i][0]) {
 				pq.add(arr[i]);
 				i++;
@@ -18,6 +22,10 @@ class Solution {
 			w+=pq.poll()[1];
 			k--;
 		}
+        while(!pq.isEmpty() && k>0){
+            w+=pq.poll()[1];
+            k--;
+        }
 		return w;
     }
 }
