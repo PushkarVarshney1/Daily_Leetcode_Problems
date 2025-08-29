fbaad46f16b92693f72d6e60b25b2aa0cd698ccc
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-08-30 00:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (86.88%) - LeetSync

---
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        long ans=1;
+        
+        // Note that only the pair having odd sum of x and y --> makes Alice win
+            //  totalPairs = n*m;
+        return ans*n*m/2;
+    }
+}
