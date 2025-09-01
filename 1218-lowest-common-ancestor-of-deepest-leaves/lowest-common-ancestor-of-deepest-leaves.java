7f416131d2a2a32bda3e6384765aed697b7682d3
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-09-02 00:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (31.49%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxd=0;
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        getDepth(root,1);
+        return fun(root,1);
+    }
+    public void getDepth(TreeNode root, int lev) {
+        if (root == null) return;
+        maxd = Math.max(maxd, lev);
+        getDepth(root.left, lev+1);
+        getDepth(root.right, lev+1);
+    }
+    public TreeNode fun(TreeNode root,int lev) {
+        if(root == null){
+            return null;
+        }
+        if(maxd == lev){
+            return root;
+        }
+        TreeNode left=fun(root.left,lev+1);
+        TreeNode right=fun(root.right,lev+1);
+        
+        if(right != null && left != null){
+            return root;
+        }
+        else if (left == null)return right;
+        else return left;
+    }
+    
+}
