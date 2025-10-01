5b9adb2ada9d9f1bcfd6c39faad656cd93a9871f
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-10-01 23:49:46 +0530

    Time: 47 ms (71.10%) | Memory: 73.4 MB (72.50%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        // Same intution as min arrow to burst ballons
+        int n=intervals.length;
+        if(n==1)return 0;
+        // Sorting by last element
+        Arrays.sort(intervals, (a,b)->a[1]-b[1]);
+        int ans=0;
+        int Edpoint=intervals[0][1];
+        for(int i=1; i<n; i++){
+            int st=intervals[i][0];
+            int ed=intervals[i][1];
+            if(st < Edpoint){
+                ans++;
+            }
+            else{
+                Edpoint=ed;
+            }
+        }
+        return ans;
+    }
+}
