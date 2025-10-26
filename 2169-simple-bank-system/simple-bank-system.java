70aedb02ae4f7aab7e4f6748d9c75feef310685f
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-10-26 22:48:24 +0530

    Time: 98 ms (55.42%) | Memory: 101.5 MB (29.76%) - LeetSync

--- 2169-simple-bank-system/simple-bank-system.java
+++ 2169-simple-bank-system/simple-bank-system.java
@@ -1,14 +1,13 @@
 class Bank {
 
-    long [] balance;
-    int n;
+    public long [] balance;
+
     public Bank(long[] balance) {
         this.balance=balance;
-        this.n=balance.length;
     }
     
     public boolean transfer(int account1, int account2, long money) {
-        if(account1 > n || account2 > n || balance[account1 - 1] < money) return false;
+        if(account1 > balance.length || account2 > balance.length || balance[account1 - 1] < money) return false;
         else if(balance[account1 - 1 ] >= money){
             balance[account1 -1] -= money;
             balance[account2 -1] += money;
@@ -18,13 +17,13 @@ class Bank {
     }
     
     public boolean deposit(int account, long money) {
-        if(account > n) return false;
+        if(account > balance.length) return false;
         balance[account -1] += money;
         return true;
     }
     
     public boolean withdraw(int account, long money) {
-        if(account > n || balance[account - 1] < money) return false;
+        if(account > balance.length || balance[account - 1] < money) return false;
         balance[account -1] -= money;
         return true;
     }
