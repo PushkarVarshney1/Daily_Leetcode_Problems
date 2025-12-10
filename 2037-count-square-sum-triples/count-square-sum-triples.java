120985e1f4a36d73384c88979a0d2123ae90261d
PushkarVarshney1 <varshneypushkar01@gmail.com>
2025-12-10 21:31:20 +0530

    Time: 15 ms (39.77%) | Memory: 42.2 MB (27.39%) - LeetSync

--- 2037-count-square-sum-triples/count-square-sum-triples.java
+++ 2037-count-square-sum-triples/count-square-sum-triples.java
@@ -1,15 +1,17 @@
 class Solution {
     public int countTriples(int n) {
 
-        // Brute force
+        // Optimize from previous ( TC -> n2 )
         int ans = 0;
         for(int i=1; i<=n; i++){
-            int a = i*i;
             for(int j=1; j<=n; j++){
-                int b = j*j;
-                for(int k=1;k<=n;k++){
-                    if((k*k) == a + b)ans++;
-                }
+                long temp = (long)(i*i) + (long)(j*j);
+                double c = Math.sqrt(temp);
+                // check if c is Integer not float then ans++
+                boolean check_C = (c == (int)c);
+                // Also check c is less than n
+                boolean Inrange = (c <= n);
+                if(Inrange == true && check_C  == true) ans++;
             }
         }
         return ans;
