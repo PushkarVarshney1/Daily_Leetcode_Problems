a9d4d71dce7793db0b595f677b2a4e51b648eeb5
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-01-06 11:54:31 +0530

    Time: 1 ms (69.68%) | Memory: 43.5 MB (47.08%) - LeetSync

--- Assignment-10/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ Assignment-10/309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -6,19 +6,19 @@ class Solution {
 
     private int dfs(int[] prices, int day, int canBuy, int[][]dp) {
         if (day >= prices.length) return 0;
+        // Memoization
+        if(dp[day][canBuy] != 0)return dp[day][canBuy];
 
         if (canBuy == 1) {
-            if(dp[day][canBuy] != 0)return dp[day][canBuy];
-
             int buy = -prices[day] + dfs(prices, day + 1, 0, dp);
             int skip = dfs(prices, day + 1, 1, dp);
+
             return dp[day][canBuy] = Math.max(buy, skip);
         }
         else {
-            if(dp[day][canBuy] != 0)return dp[day][canBuy];
-            
             int sell = prices[day] + dfs(prices, day + 2, 1, dp); // cooldown tomorrow
             int hold = dfs(prices, day + 1, 0, dp);
+            
             return dp[day][canBuy] = Math.max(sell, hold);
         }
     }
