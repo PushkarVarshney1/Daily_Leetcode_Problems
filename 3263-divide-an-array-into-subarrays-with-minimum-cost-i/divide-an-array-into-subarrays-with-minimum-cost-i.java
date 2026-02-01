de034491d155549c3833c2e62003de833d2e18c1
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-02-01 16:59:41 +0530

    Time: 5 ms (22.80%) | Memory: 45.9 MB (17.20%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -1,12 +1,8 @@
 class Solution {
     public int minimumCost(int[] nums) {
-        int i = 1 , j = nums.length-1;
-        int min = Integer.MAX_VALUE;
-        while(i < j){
-            min = Math.min(min, nums[i]+nums[j]);
-            if(nums[i] > nums[j])i++;
-            else j--;
-        }
-        return nums[0] + min;
+        // Not To sort the whole array b/c the first ele is always be the part of answer.
+        // So sort the array from index 1 to last.
+        Arrays.sort(nums, 1, nums.length);
+        return nums[0] + nums[1] + nums[2];
     }
 }
