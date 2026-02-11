e723a46f7f54535451a803b8a927f27f863b579a
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-02-11 12:44:58 +0530

    Time: 426 ms (93.69%) | Memory: 160.5 MB (89.51%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -0,0 +1,74 @@
+class Solution {
+    public int findMaximumXOR(int[] arr) {
+        Trie t = new Trie();
+		for(int i : arr) {
+			t.add(i);
+		}
+		int ans = 0;
+		for(int x : arr) {
+			ans = Math.max(t.getMaxXOR(x), ans);
+		}
+		return ans;
+    }
+    class Trie{
+		class Node{
+			Node zero;
+			Node one;
+        }
+		
+		private Node root = new Node();
+		
+        public void add(int val) {
+        	Node curr = root;
+        	for(int i=31; i>=0; i--) {
+        		int bit = val & (1 << i);
+        		if(bit == 0) {
+        			if(curr.zero != null) {
+        				curr = curr.zero;
+        			}
+        			else {
+        				Node nn = new Node();
+        				curr.zero = nn;
+        				curr = nn;
+        			}
+        		}
+        		else {
+        			if(curr.one != null) {
+        				curr = curr.one;
+        			}
+        			else {
+        				Node nn = new Node();
+        				curr.one = nn;
+        				curr = nn;
+        			}
+        		}
+        	}
+        }
+        public int getMaxXOR(int x) {
+        	int ans = 0;
+        	Node curr = root;
+        	for(int i=31; i>=0; i--) {
+        		int bit = x & (1<<i);
+        		if(bit == 0) {
+        			if(curr.one !=  null){
+        				ans = ans | (1<<i);
+        				curr = curr.one;
+        			}
+        			else {
+        				curr = curr.zero;
+        			}
+        		}
+        		else{
+        			if(curr.zero != null){
+        				ans = ans | (1<<i);
+        				curr = curr.zero;
+        			}
+        			else {
+        				curr = curr.one;
+        			}
+        		}
+        	}
+        	return ans;
+        }
+	}
+}
