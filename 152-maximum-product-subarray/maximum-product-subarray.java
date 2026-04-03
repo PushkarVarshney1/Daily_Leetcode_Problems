8d0817a9e3e56719fa74fefdb642061f31bb62d7
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-04-03 14:53:57 +0530

    Time: 2 ms (69.06%) | Memory: 47.4 MB (22.54%) - LeetSync

---
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        // Modified Kadanes
+        int ans = nums[0];
+        int max = nums[0] , min = nums[0];
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] < 0){
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(nums[i], nums[i] * max);
+            min = Math.min(nums[i], nums[i] * min);
+            // answer have to store every time 
+            ans = Math.max(max, ans);
+        }
+        return ans;
+    }
+}
