29ded26e79092c157d9c3b5c8e6700d9b58ee298
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-04-05 21:52:45 +0530

    Time: 4 ms (99.05%) | Memory: 45.9 MB (48.28%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -1,23 +1,18 @@
 class Solution {
-    // Using recursion
     public boolean judgeCircle(String moves) {
-        return movement(moves, 0, 0,0);
-    }
-    
-    boolean movement(String s,int idx , int x, int y){
-        if(idx >= s.length()){
-            return (x==0 && y==0);
+        int x = 0, y = 0;
+        for(char ch : moves.toCharArray()){
+            if(ch == 'U'){
+               y++;
+            }
+            else if(ch == 'D'){
+                y--;
+            }
+            else if(ch == 'L'){
+                x--;
+            }
+            else x ++;
         }
-        char ch = s.charAt(idx);
-        if(ch == 'U'){
-           return movement(s, idx + 1, x, y+1);
-        }
-        else if(ch == 'D'){
-            return movement(s, idx + 1, x, y-1);
-        }
-        else if(ch == 'L'){
-            return movement(s, idx + 1, x-1, y);
-        }
-        return movement(s, idx + 1, x+1, y);
-    }
+        return (x == 0 && y == 0);
+    }   
 }
