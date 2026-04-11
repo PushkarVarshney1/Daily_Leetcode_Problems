e0398fa8505c609747229d735f614080609ccba5
PushkarVarshney1 <varshneypushkar01@gmail.com>
2026-04-11 23:25:59 +0530

    Time: 4 ms (49.26%) | Memory: 43.6 MB (65.14%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -1,21 +1,21 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        int[] freq1 = new int[10];
-        int[] freq2 = new int[10];
+        int[] freq = new int[10];
         int bull = 0;
+        int cow = 0;
         for(int i=0; i<secret.length(); i++){
-            if(secret.charAt(i) == guess.charAt(i)){
+            int a = secret.charAt(i) - '0';
+            int b = guess.charAt(i) - '0';
+            if(a == b){
                 bull++;
             }
             else{
-                freq1[secret.charAt(i) - '0']++;
-                freq2[guess.charAt(i) - '0']++;
+                if(freq[a] < 0)cow++;
+                if(freq[b] > 0)cow++;
+                freq[a]++;
+                freq[b]--;
             }
         }
-        int cow = 0;
-        for(int i=0; i<10; i++){
-            cow += Math.min(freq1[i] , freq2[i]);
-        }
-        return bull+"A"+cow+"B";
+        return bull + "A" + cow + "B";
     }
 }
